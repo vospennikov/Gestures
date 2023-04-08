45aa1a5e4d840ac5bf31e9fb3c3df9dc1b71d47a
Mikhail Vospennikov <m.vospennikov@gmail.com>
2023-04-08 10:30:24 +0400

    Update example

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -9,21 +9,43 @@ import SwiftUI
 import Gestures
 
 struct ContentView: View {
+    @State var boxPosition = CGPoint(x: 200, y: 200)
+    @State var fingerPosition: CGPoint?
+    
     @State var touchLocation: String = "None"
     @State var swipeDirection: String = "None"
     
     var body: some View {
         ZStack {
-            Rectangle()
-                .foregroundColor(.white)
+            Color.white
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .onTouchGesture(count: 1) { gesture in
                     touchLocation = gesture.location.roundedDescription
+                    boxPosition = gesture.location
+                    fingerPosition = gesture.location
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
+                        fingerPosition = nil
+                    }
                 }
                 .onSwipeGesture(minimumDistance: 15.0, coordinateSpace: .local) { direction, location in
                     swipeDirection = "\(direction): \(location.roundedDescription)"
+                    fingerPosition = location
                 } onEnded: { direction, location in
                     swipeDirection = "\(direction): \(location.roundedDescription)"
+                    fingerPosition = nil
+                    switch direction {
+                        case .up: boxPosition.y -= 50
+                        case .down: boxPosition.y += 50
+                        case .left: boxPosition.x -= 50
+                        case .right: boxPosition.x += 50
+                        default: break
+                    }
                 }
+            
+            RoundedRectangle(cornerRadius: 16, style: .continuous)
+                .foregroundColor(.pink)
+                .frame(width: 75, height: 75)
+                .position(boxPosition)
             
             HStack {
                 VStack(alignment: .leading) {
@@ -35,7 +57,16 @@ struct ContentView: View {
                 Spacer()
             }
             .padding()
+            
+            if let fingerPosition {
+                Circle()
+                    .fill(Color(red: 0.7, green: 0.7, blue: 0.7))
+                    .frame(width: 44, height: 44)
+                    .shadow(color: .gray.opacity(0.7), radius: 8, x: 4, y: 4)
+                    .position(fingerPosition)
+            }
         }
+        .animation(.spring(), value: boxPosition)
     }
 }
 
